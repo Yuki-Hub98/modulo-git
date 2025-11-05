c8ceb6cc7a8d8d2d4f0ef9159a7b43f2b142a423
Hamilton <hamiltondev@hotmail.com>
2025-11-05 19:27:25 -0400

    feat(criar mensagem git) - Implementação para criar mensagem de commit

--- desafios.py
+++ desafios.py
@@ -21,13 +21,7 @@ def listar_comandos_git_basicos():
 
 
 def criar_mensagem_commit(funcao_nome):
-    """
-    Recebe o nome de uma função e retorna uma mensagem de commit padronizada.
-    Exemplo:
-    criar_mensagem_commit("listar_comandos_git_basicos") ->
-    "Implementa função listar_comandos_git_basicos"
-    """
-    pass
+    print(f"Implementa função {funcao_nome}")
 
 
 def verificar_tag_valida(tag):
